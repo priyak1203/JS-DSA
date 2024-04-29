41f541364e84d962f2761be6a435f2bd4e5745e5
Priydarshini Karanth <kpriya1203@gmail.com>
2024-04-29 21:35:37 +0530

    bubble sort

--- 1.Algorithms/12.BubbleSort.js
+++ 1.Algorithms/12.BubbleSort.js
@@ -6,7 +6,6 @@ function bubbleSort(arr) {
   let swapped;
   do {
     swapped = false;
-
     for (let i = 0; i < arr.length - 1; i++) {
       if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
@@ -21,3 +20,6 @@ function bubbleSort(arr) {
 const arr = [8, 20, -2, 4, -6];
 bubbleSort(arr);
 console.log(arr); // [ -6, -2, 4, 8, 20 ]
+
+// Big O
+// O(n^2) - Quadratic
